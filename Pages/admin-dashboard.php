9dc89d6f5f014231c5abda90555e01f7c951ad29
Chuajerome26 <jerome.chua06@gmail.com>
2023-10-23 14:34:39 +0800

    update

--- Pages/admin-dashboard.php
+++ Pages/admin-dashboard.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title>Admin Dashboard</title>
-    <link rel="stylesheet" type="text/css" href="adminstyle.css">
+    <link rel="stylesheet" type="text/css" href="../assets/adminstyle.css">
 
     <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
