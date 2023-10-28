f26649e596190dfcc97fc63599e91ec013bfb0e3
Ericson21 <99259314+Ericson21@users.noreply.github.com>
2023-10-28 19:08:13 +0800

    ProfileCard
    Not done yet

---
+++ Pages-admin/profilecard.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="icon" href="../images/consuelo.jpg" type="image/x-icon">
+    <link rel="stylesheet" href="../assets/style1.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet">
+
+
+
+
+    <title>Scholar's Profile</title>
+    <link rel="stylesheet" href="../assets/pcard.css" />
+    <link rel="stylesheet"  href="style.css">
+
+  
+<!--navbar-->
+<nav class="navbar">
+      <div class="logo_item">
+        <i class="" id="sidebarOpen"></i>
+       &nbsp; &nbsp;</i>Logo's Here
+      </div>
+
+   
+ 
+
+      <div class="navbar_content">
+        <i class="bi bi-grid"></i>
+        <i class='bx bx-sun' id="darkLight"></i>
+        <i class='bx bx-bell' ></i>
+        
+        <img src="john.jpg" alt="" class="profile" />
+        
+      </div>
+    </nav>
+
+
+</head>
+
+<body>
+  <!--dropdown-->
+<div align="right" class="dropdown">
+  
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+    Dropdown button
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+  </ul>
+</div>
+<br>
+
+
+  <!--Cards-->
+ 
+  <div class="card" style="width: 18rem;   ">
+  <img src="../images/pcard1.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h2 class="card-title">Antukin, Cong</h2>
+    <h2 class="card-title">Student</h2>
+    <p class="card-text">QCU-20-2132</p>
+    <a href="#" class="btn btn-primary">More Details</a>
+  </div>
+</div>
+</div>
+
+
+<div class="card" style="width: 18rem;">
+  <img src="../images/pcard2.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+  <h2 class="card-title">Ankle, Bradpitt</h2>
+    <h2 class="card-title">Student</h2>
+    <p class="card-text">QCU-21-2014</p>
+    <a href="#" class="btn btn-primary">More Details</a>
+  </div>
+</div>
+
+
+<div class="card" style="width: 18rem;">
+<img src="../images/pcard3.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+  <h2 class="card-title">Anneong, Haseyo</h2>
+    <h2 class="card-title">Student</h2>
+    <p class="card-text">QCU-20-2178</p>
+    <a href="#" class="btn btn-primary">More Details</a>
+  </div>
+</div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" 
+    crossorigin="anonymous"></script>
+
+
+    
+    
+  </body>
+</html>
