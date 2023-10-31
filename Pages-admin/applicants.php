243cc7cd96b539d7282e4210cf5dd5fac5689302
Chuajerome26 <jerome.chua06@gmail.com>
2023-10-31 08:24:15 +0800

    update

--- Pages-admin/applicants.php
+++ Pages-admin/applicants.php
@@ -1,10 +1,13 @@
-
+<?php
+include("header.php"); 
+?>
 <html>
-<title>Applicants</title>
+<head>    
+    <title>Applicants</title>
     <h1><center>Applicants</center></h1>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-
+    <link rel="stylesheet" href="../assets/NewApplicant-style.css">
+</header>
 <body>
 <div class="table-box">
         <div class="table-row table-head">
