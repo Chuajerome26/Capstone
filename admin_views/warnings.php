598ed7e267edfed237e5d8f461e5d8b268c6cf3b
Chuajerome26 <jerome.chua06@gmail.com>
2023-11-09 12:19:47 +0800

    updates

---
+++ admin_views/warnings.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .container{
+            height: 500px;
+            width: 400px;
+            border: 1px solid black;
+            border-radius: 20px;
+            justify-content: center;
+        }
+        table,th,td{
+            border:1px solid black;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <h2>Warnings</h2>
+        </div>
+        <table>
+            <tr>
+                <th>Scholar</th>
+                <th>Percentage</th>
+            </tr>
+            <tr>
+                <td>A</td>
+                <td>A</td>
+            </tr>
+        </table>    
+    </div>
+</body>
+</html> 
