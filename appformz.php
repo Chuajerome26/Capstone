2752f11eb52cfd734500c8eda16ecf304b0f45ad
En1amreg <germaine.indias23@gmail.com>
2023-11-16 03:02:51 +0800

    appform
    shessh

---
+++ appformz.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+
+
+<title>CCMF FORM</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<style type="text/css">
+    	body {
+    margin: 0;
+    padding-top: 40px;
+    color: #2e323c;
+    background: #f5f6fa;
+    position: relative;
+    height: 100%;
+}
+.account-settings .user-profile {
+    margin: 0 0 1rem 0;
+    padding-bottom: 1rem;
+    text-align: center;
+}
+.account-settings .user-profile .user-avatar {
+    margin: 0 0 1rem 0;
+}
+.account-settings .user-profile .user-avatar img {
+    width: 90px;
+    height: 90px;
+    -webkit-border-radius: 100px;
+    -moz-border-radius: 100px;
+    border-radius: 100px;
+}
+.account-settings .user-profile h5.user-name {
+    margin: 0 0 0.5rem 0;
+}
+.account-settings .user-profile h6.user-email {
+    margin: 0;
+    font-size: 0.8rem;
+    font-weight: 400;
+    color: #9fa8b9;
+}
+.account-settings .about {
+    margin: 2rem 0 0 0;
+    text-align: center;
+}
+.account-settings .about h5 {
+    margin: 0 0 15px 0;
+    color: #007ae1;
+}
+.account-settings .about p {
+    font-size: 0.825rem;
+}
+.form-control {
+    border: 1px solid #cfd1d8;
+    -webkit-border-radius: 2px;
+    -moz-border-radius: 2px;
+    border-radius: 2px;
+    font-size: .825rem;
+    background: #ffffff;
+    color: #2e323c;
+}
+
+.card {
+    background: #ffffff;
+    -webkit-border-radius: 5px;
+    -moz-border-radius: 5px;
+    border-radius: 5px;
+    border: 0;
+    margin-bottom: 1rem;
+}
+
+
+    </style>
+</head>
+<div class="container mt-4">
+    <div class="row mx-auto">
+      <div class="col-xl-1 col-lg-6 col-md-6 col-sm-6 col-6">
+        <img src="images/consuelo.jpg" alt="Image" class="img-fluid">
+      </div>
+      <div class="col">
+        <header>
+          <h1 class="display-7 text-center">Consuelo Chito Madrigal Foundation (CCMF)</h1>
+        </header>
+      </div>
+    </div>
+  </div>
+<body>
+<div class="col-xl-9 col-lg-9 col-md-12 col-sm-12 col-12 mx-auto text-center">
+<div class="card h-100">
+<div class="card-body">
+<div class="row gutters">
+<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+<h6 class="mb-2 text-primary">Personal's Information</h6>
+</div>
+<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
+<div class="form-group">
+<label for="Fname">First Name:</label>
+<input type="text" class="form-control" id="Fname" placeholder="First Name">
+</div>
+</div>
+<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
+<div class="form-group">
+<label for="Lname">Last Name:</label>
+<input type="Lname" class="form-control" id="Lname" placeholder="Last Name">
+</div>
+</div>
+<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
+    <div class="form-group">
+    <label for="gend">Gender:</label>
+    <input type="gend" class="form-control" id="gend" placeholder="Gender">
+    </div>
+    </div>
+    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
+    <div class="form-group">
+        <label for="Cstat">Civil Status:</label>
+        <input type="Cstat" class="form-control" id="Cstat" placeholder="Civil Status">
+        </div>
+        </div>
+<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
+<div class="form-group">
+<label for="Cship">Citizenship</label>
+<input type="Cship" class="form-control" id="Cship" placeholder="Enter Citizenship">
+</div>
+</div>
+<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
+<div class="form-group">
+<label for="website">Date of Birth:</label>
+<input type="url" class="form-control" id="website" placeholder="zz">
+</div>
+</div>
+<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
+    <div class="form-group">
+    <label for="Bplace">Birthplace:</label>
+    <input type="Bplace" class="form-control" id="Bplace" placeholder="Birthplace">
+    </div>
+    </div>
+    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
+        <div class="form-group">
+        <label for="rgion">Religion:</label>
+        <input type="rgion" class="form-control" id="rgion" placeholder="Religion">
+        </div>
+        </div>
+        <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
+            <div class="form-group">
+            <label for="Mnum">Mobile Number:</label>
+            <input type="Mnum" class="form-control" id="Mnum" placeholder="Enter # Number">
+            </div>
+            </div>
+            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
+                <div class="form-group">
+                <label for="Eadd">Email Address:</label>
+                <input type="Eadd" class="form-control" id="Eadd" placeholder="Email Address">
+                </div>
+                </div>
+                <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="form-group">
+                    <label for="adds">Address:</label>
+                    <input type="adds" class="form-control" id="adds" placeholder="Address">
+                    </div>
+                    </div>
+</div>
+<div class="row gutters">
+<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+<h6 class="mt-3 mb-2 text-primary">Grade Information</h6>
+</div>
+<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
+    <div class="form-group">
+    <label for="tsubj">Total Subject:</label>
+    <input type="tsubj" class="form-control" id="tsubj" placeholder="Total Subject">
+    </div>
+    </div>
+
+    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
+        <div class="form-group">
+        <label for="Tunit">Total Units:</label>
+        <input type="Tunit" class="form-control" id="Tunit" placeholder="Total Units">
+        </div>
+        </div>
+<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
+<div class="form-group">
+<label for="genwa">General Weighted Average:</label>
+<input type="genwa" class="form-control" id="genwa" placeholder="GWA">
+</div>
+</div>
+</div>
+<div class="row gutters">
+<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+<h6 class="mt-3 mb-2 text-primary">Requirements</h6>
+</div>
+<div class="row gutters">
+<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
+    <div class="form-group">
+    <label for="Fname">Upload 2X2 ID Photo</label>
+    <label for="formFile" class="form-label"></label>
+    <input class="form-control" type="file" id="formFile">
+    </div>
+    </div>
+    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
+        <div class="form-group">
+        <label for="Tunit">Latest Copy of Grades:</label>
+        <label for="formFile" class="form-label"></label>
+        <input class="form-control" type="file" id="formFile">
+        </div>
+        </div>
+        <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
+            <div class="form-group">
+            <label for="Tunit">Copy of Birth Certificate/PSA</label>
+            <label for="formFile" class="form-label"></label>
+            <input class="form-control" type="file" id="formFile">
+            </div>
+            </div>
+            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
+                <div class="form-group">
+                <label for="Tunit">Certificate of Good Moral</label>
+                <label for="formFile" class="form-label"></label>
+                <input class="form-control" type="file" id="formFile">
+                </div>
+                </div>
+                    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
+                        <div class="form-group">
+                        <label for="Fname">Copy of Enrollment Form </label>
+                        <label for="formFile" class="form-label"></label>
+                        <input class="form-control" type="file" id="formFile">
+                        </div>
+                        </div>
+    </div>
+</div>
+<div class="row gutters">
+<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+<div class="text-right">
+<button type="button" id="submit" name="submit" class="btn btn-secondary">Cancel</button>
+<button type="button" id="submit" name="submit" class="btn btn-primary">Submit</button>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script type="text/javascript">
+
+</script>
+</body>
+</html>
