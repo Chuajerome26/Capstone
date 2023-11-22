672e2d496f28a4a88dc3a05c705447a658a3950d
En1amreg <germaine.indias23@gmail.com>
2023-11-22 21:37:08 +0800

    Update appforms.php

--- Pages-scholar/appforms.php
+++ Pages-scholar/appforms.php
@@ -75,176 +75,178 @@
 
     </style>
 </head>
-<!-- $scholarData = array(
-        'f_name' => trim($_POST["f_name"]),
-        'l_name' => trim($_POST["l_name"]),
-        'gender' => trim($_POST["gender"]),
-        'cStatus' => trim($_POST["cStatus"]),
-        'citizenship' => trim($_POST["citizenship"]),
-        'bday' => trim($_POST["bday"]),
-        'bplace' => $_POST["bplace"],
-        'religion' => $_POST["religion"],
-        'mNum' => $_POST["mNum"],
-        'email' => $_POST["email"],
-        'address' => $_POST["address"],
-        'totalSub' => trim($_POST["totalSub"]),
-        'totalUnits' => trim($_POST["totalUnits"]),
-        'gwa' => trim($_POST["gwa"]),
-    ); -->
 
+
+<body class="">
+    
 <div class="container mt-4">
-    <div class="row mx-auto">
+    <div class="d-flex justify-content-center align-items-center">
       <div class="col-xl-1 col-lg-6 col-md-6 col-sm-6 col-6">
         <img src="../images/consuelo.jpg" alt="Image" class="img-fluid">
       </div>
-      <div class="col">
+      <div class="">
         <header>
           <h1 class="display-7 text-center">Consuelo Chito Madrigal Foundation (CCMF)</h1>
         </header>
       </div>
     </div>
-  </div>
-<body>
-<div class="col-xl-9 col-lg-9 col-md-12 col-sm-12 col-12 mx-auto text-center">
+</div>
+
+<form action="../functions/applicants-register.php" method="post" enctype="multipart/form-data" name="myForm" onsubmit="return validateForm()"> 
+<div class="container col-xl-9 col-lg-9 col-md-12 col-sm-12 col-12 mx-auto mt-3">
 <div class="card h-100">
 <div class="card-body">
 <div class="row gutters">
+
+<!-- PERSONAL INFORMATION -->
 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-<h6 class="mb-2 text-primary">Personal's Information</h6>
-</div>
-<form action="../functions/applicants-register.php" method="post" enctype="multipart/form-data" name="myForm" onsubmit="return validateForm()"> 
-<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
-<div class="form-group">
-<label for="Fname">First Name:</label>
-<input type="text" class="form-control" name="f_name" placeholder="First Name" value="<?php echo isset($_POST['f_name']) ? htmlspecialchars($_POST['f_name']) : ''; ?>" required>
-</div>
+    <h4 class="mb-2 text-primary">Personal's Information</h4>
+    <div class="border-bottom container mb-3 border border-1"></div>
 </div>
 <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
-<div class="form-group">
-<label for="Lname">Last Name:</label>
-<input type="text" class="form-control" name="l_name" placeholder="Last Name" value="<?php echo isset($_POST['l_name']) ? htmlspecialchars($_POST['l_name']) : ''; ?>" required>
+    <div class="form-group">
+        <label for="Fname" class="font-weight-bold">First Name:</label>
+        <input type="text" class="form-control" name="f_name" placeholder="First Name" value="<?php echo isset($_POST['f_name']) ? htmlspecialchars($_POST['f_name']) : ''; ?>" required>
+    </div>
+</div>
+
+<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
+    <div class="form-group">
+        <label for="Lname" class="font-weight-bold">Last Name:</label>
+        <input type="text" class="form-control" name="l_name" placeholder="Last Name" value="<?php echo isset($_POST['l_name']) ? htmlspecialchars($_POST['l_name']) : ''; ?>" required>
+    </div>
+</div>
+<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
+    <div class="form-group">
+        <?php echo isset($_POST['gender']) ? htmlspecialchars($_POST['gender']) : ''; ?>
+        <label for="gender" class="font-weight-bold">Gender:</label>
+        <select class="form-control" id="exampleFormControlSelect1">
+            <option>Male</option>
+            <option>Female</option>
+            <option>Others</option>
+</select>
 </div>
 </div>
 <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
     <div class="form-group">
-    <label for="gend">Gender:</label>
-    <input type="text" class="form-control" name="gender" placeholder="Gender" value="<?php echo isset($_POST['gender']) ? htmlspecialchars($_POST['gender']) : ''; ?>" required>
-    </div>
-    </div>
-    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
-    <div class="form-group">
-        <label for="Cstat">Civil Status:</label>
+        <label for="Cstat" class="font-weight-bold">Civil Status:</label>
         <input type="text" class="form-control" name="cStatus" placeholder="Civil Status" value="<?php echo isset($_POST['cStatus']) ? htmlspecialchars($_POST['cStatus']) : ''; ?>" required>
-        </div>
-        </div>
+    </div>
+</div>
 <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
 <div class="form-group">
-<label for="Cship">Citizenship</label>
+<label for="Cship" class="font-weight-bold" >Citizenship</label>
 <input type="text" class="form-control" name="citizenship" placeholder="Enter Citizenship" value="<?php echo isset($_POST['citizenship']) ? htmlspecialchars($_POST['citizenship']) : ''; ?>" required>
 </div>
 </div>
 <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
 <div class="form-group">
-<label for="website">Date of Birth:</label>
+<label for="website" class="font-weight-bold" >Date of Birth:</label>
 <input type="date" class="form-control" name="bday" value="<?php echo isset($_POST['bday']) ? htmlspecialchars($_POST['bday']) : ''; ?>" required>
 </div>
 </div>
 <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
     <div class="form-group">
-    <label for="Bplace">Birthplace:</label>
+    <label for="Bplace" class="font-weight-bold" >Birthplace:</label>
     <input type="text" class="form-control" name="bplace" placeholder="Birthplace" value="<?php echo isset($_POST['bplace']) ? htmlspecialchars($_POST['bplace']) : ''; ?>" required>
     </div>
     </div>
     <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
         <div class="form-group">
-        <label for="rgion">Religion:</label>
+        <label for="rgion" class="font-weight-bold">Religion:</label>
         <input type="text" class="form-control" name="religion" placeholder="Religion" value="<?php echo isset($_POST['religion']) ? htmlspecialchars($_POST['religion']) : ''; ?>" required>
         </div>
         </div>
         <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
             <div class="form-group">
-            <label for="Mnum">Mobile Number:</label>
-            <input type="text" class="form-control" name="mNum" placeholder="Enter # Number" value="<?php echo isset($_POST['mNUm']) ? htmlspecialchars($_POST['mNum']) : ''; ?>" onkeydown="return onlyNumberKey(event)" required>
+            <label for="Mnum" class="font-weight-bold">Mobile Number:</label>
+            <input type="number" class="form-control" name="mNum" placeholder="Enter Mobile Number" value="<?php echo isset($_POST['mNUm']) ? htmlspecialchars($_POST['mNum']) : ''; ?>" onkeydown="return onlyNumberKey(event)" required>
             </div>
             </div>
             <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
                 <div class="form-group">
-                <label for="Eadd">Email Address:</label>
-                <input type="text" class="form-control" name="email" placeholder="Email Address" value="<?php echo isset($_POST['email']) ? htmlspecialchars($_POST['email']) : ''; ?>" required>
+                <label for="Eadd" class="font-weight-bold">Email Address:</label>
+                <input type="email" class="form-control" name="email" placeholder="Email Address" value="<?php echo isset($_POST['email']) ? htmlspecialchars($_POST['email']) : ''; ?>" required>
                 </div>
                 </div>
                 <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
                     <div class="form-group">
-                    <label for="adds">Address:</label>
+                    <label for="adds" class="font-weight-bold">Address:</label>
                     <input type="text" class="form-control" name="address" placeholder="Address" value="<?php echo isset($_POST['address']) ? htmlspecialchars($_POST['address']) : ''; ?>" required>
                     </div>
                     </div>
-</div>
 <div class="row gutters">
-<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-<h6 class="mt-3 mb-2 text-primary">Grade Information</h6>
-</div>
-<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
-    <div class="form-group">
-    <label for="tsubj">Total Subject:</label>
-    <input type="text" class="form-control" name="totalSub" placeholder="Total Subject" value="<?php echo isset($_POST['totalSub']) ? htmlspecialchars($_POST['totalSub']) : ''; ?>" required>
-    </div>
+
+    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+        <h4 class="mt-3 mb-2 text-primary">Grade Information</h4>
+        <div class="border-bottom container mb-3 border border-1"></div>
     </div>
 
     <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
         <div class="form-group">
-        <label for="Tunit">Total Units:</label>
-        <input type="text" class="form-control" name="totalUnits" placeholder="Total Units" value="<?php echo isset($_POST['totalUnits']) ? htmlspecialchars($_POST['totalUnits']) : ''; ?>" required>
+            <label for="tsubj" class="font-weight-bold">Total Subject:</label>
+            <input type="number" class="form-control" name="totalSub" placeholder="Total Subject" value="<?php echo isset($_POST['totalSub']) ? htmlspecialchars($_POST['totalSub']) : ''; ?>" required>
         </div>
+    </div>
+
+    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
+        <div class="form-group">
+            <label for="Tunit" class="font-weight-bold">Total Units:</label>
+            <input type="number" class="form-control" name="totalUnits" placeholder="Total Units" value="<?php echo isset($_POST['totalUnits']) ? htmlspecialchars($_POST['totalUnits']) : ''; ?>" required>
         </div>
-<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
-<div class="form-group">
-<label for="genwa">General Weighted Average:</label>
-<input type="text" class="form-control" name="gwa" placeholder="GWA" value="<?php echo isset($_POST['gwa']) ? htmlspecialchars($_POST['gwa']) : ''; ?>" required> 
-</div>
-</div>
-</div>
+    </div>
+
+    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
+        <div class="form-group">
+            <label for="genwa" class="font-weight-bold">General Weighted Average:</label>
+            <input type="number" class="form-control" name="gwa" placeholder="GWA" value="<?php echo isset($_POST['gwa']) ? htmlspecialchars($_POST['gwa']) : ''; ?>" required> 
+        </div>
+    </div>
 <div class="row gutters">
-<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-<h6 class="mt-3 mb-2 text-primary">Requirements</h6>
-</div>
+
+    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+        <h4 class="mt-3 mb-2 text-primary" class="font-weight-bold">Requirements</h4>
+    </div>
+
+    <div class="border-bottom container mb-3 border border-1"></div>
+
 <div class="row gutters">
 <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
     <div class="form-group">
-    <label for="Fname">Upload 2X2 name Photo</label>
-    <label for="formFile" class="form-label"></label>
+    <label for="Fname" class="font-weight-bold">Upload 2X2 Name Photo</label>
+    <!-- <label for="formFile" class="form-label"></label> -->
     <input class="form-control" type="file" name="idPhoto" accept=".jpeg, .jpg, .png, .pdf" required>
     </div>
-    </div>
+</div>
     <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
         <div class="form-group">
-        <label for="Tunit">Latest Copy of Grades:</label>
-        <label for="formFile" class="form-label"></label>
+        <label for="Tunit" class="font-weight-bold">Latest Copy of Grades:</label>
+        <!-- <label for="formFile" class="form-label"></label> -->
         <input class="form-control" type="file" name="grades" accept=".jpeg, .jpg, .png, .pdf" required>
         </div>
         </div>
         <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
             <div class="form-group">
-            <label for="Tunit">Copy of Birth Certificate/PSA</label>
-            <label for="formFile" class="form-label"></label>
+            <label for="Tunit" class="font-weight-bold">Copy of Birth Certificate/PSA</label>
+            <!-- <label for="formFile" class="form-label"></label> -->
             <input class="form-control" type="file" name="PSA" accept=".jpeg, .jpg, .png, .pdf" required>
             </div>
             </div>
             <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
                 <div class="form-group">
-                <label for="Tunit">Certificate of Good Moral</label>
-                <label for="formFile" class="form-label"></label>
+                <label for="Tunit" class="font-weight-bold">Certificate of Good Moral</label>
+                <!-- <label for="formFile" class="form-label"></label> -->
                 <input class="form-control" type="file" name="goodMoral" accept=".jpeg, .jpg, .png, .pdf" required>
                 </div>
                 </div>
                     <div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-12">
                         <div class="form-group">
-                        <label for="Fname">Copy of Enrollment Form </label>
+                        <label for="Fname" class="font-weight-bold">Copy of Enrollment Form </label>
                         <label for="formFile" class="form-label"></label>
                         <input class="form-control" type="file" name="eForm" accept=".jpeg, .jpg, .png, .pdf" required>
                         </div>
                         </div>
-    </div>
+</div>
 </div>
 <div class="row gutters">
 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
@@ -257,8 +259,7 @@
 </div>
 </div>
 </div>
-</div>
-</div>
+
 <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
 <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/js/bootstrap.bundle.min.js"></script>
