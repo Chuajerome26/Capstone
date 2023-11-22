27d9fb7f98f988122ba24c1f6ab6fe5763aa686e
En1amreg <germaine.indias23@gmail.com>
2023-11-22 20:14:30 +0800

    del
    updating delete

--- Pages-scholar/scholardash.php
+++ Pages-scholar/scholardash.php
@@ -33,7 +33,6 @@
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="scholardash.php">
                 <div class="sidebar-brand-icon rotate-n-15">
-                    
                 </div>
                 <div class="sidebar-brand-text mx-3">CCMF</div>
             </a>
@@ -309,8 +308,8 @@
                     <!-- Content Row -->
                     <div class="row">
 
-                            <!-- Sweat Equity Example -->
-                                                <div class="col-xl-4 col-md-4 mb-4 ">
+                        <!-- Sweat Equity Example -->
+                        <div class="col-xl-4 col-md-4 mb-4 ">
                             <div class="card border-left-primary shadow h-100 py-2">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
@@ -335,33 +334,33 @@
                             </div>
                         </div>
 
-                         <!-- Allowance Example -->
-                         <div class="col-xl-4 col-md-4 mb-4 ">
+                        <!-- Allowance Example -->
+                        <div class="col-xl-4 col-md-4 mb-4 ">
                             <div class="card border-left-primary shadow h-100 py-2">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-5" style="font-size: 22px;">
                                                 Allowance</div>
-                                                <div>
+                                                    <div>
                                                 <form action="allowancehistory.php" method="get">
                                     <button type="submit" class="btn btn-primary">View Details</button>
                                     </form>
-                                                    </div>
+</div>
                                             <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                                           
-
+                                            
+                                    
                                         </div>
-                                        
+
                                     </div>
                                 </div>
                             </div>
                         </div>
 
-                         <!-- Renewal Example -->
+                        <!-- Renewal Example -->
                          <div class="col-xl-4 col-md-4 mb-4 ">
                             <div class="card border-left-primary shadow h-100 py-2">
                                 <div class="card-body">
@@ -388,7 +387,7 @@
                         </div>
             
 
-                        
+                  
 
                         <!-- Content Column -->
                         <div class="col-lg-10 mb-4">
