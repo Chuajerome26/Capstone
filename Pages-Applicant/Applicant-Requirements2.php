0e1e1a92310606e2148427664c31cac9a3836d8f
Chuajerome26 <jerome.chua06@gmail.com>
2024-03-11 15:21:02 +0800

    IDPIC

--- Pages-Applicant/Applicant-Requirements2.php
+++ Pages-Applicant/Applicant-Requirements2.php
@@ -12,6 +12,8 @@ if (isset($_SESSION['id']) && $_SESSION['user_type'] === 0) {
     $info = $admin->getApplicantById($id);
     $pic = $admin->getApplicants2x2($id);
 
+    var_dump($pic);
+
 
 } else {
     header("Location: ../index.php");
@@ -219,7 +221,7 @@ if (isset($_SESSION['id']) && $_SESSION['user_type'] === 0) {
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $info[0]['l_name']; ?></span>
                                 <img class="img-profile rounded-circle"
-                                    src="Scholar_files/<?php echo $pic[0]['file_name']; ?>">
+                                    src="../Scholar_files/<?php echo $pic[0]['file_name']; ?>">
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
