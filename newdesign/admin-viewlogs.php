eed47961cdb9d45fdf8ded1c3f9143066dbf9ee8
Chuajerome26 <jerome.chua06@gmail.com>
2024-05-08 02:47:31 +0800

    fixing title and logo

--- newdesign/admin-viewlogs.php
+++ newdesign/admin-viewlogs.php
@@ -15,6 +15,7 @@ if (isset($_SESSION['id']) && $_SESSION['user_type'] === 6) {
     $admin_info = $admin->adminInfo($id);
     $admin_logs = $admin->getAdminlogs();
     $count = count($admin_logs);
+    $content = $admin->getContent();
 
 } else {
     header("Location: ../index.php");
@@ -25,8 +26,8 @@ if (isset($_SESSION['id']) && $_SESSION['user_type'] === 6) {
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="shortcut icon" type="image/x-icon" href="../images/forcert1.png" />
-        <title>Scholarship Management System</title>
+        <link rel="shortcut icon" type="image/x-icon" href="../images/<?php echo $content[0]['logo']; ?>" />
+        <title><?php echo $content[0]['title_name']; ?></title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
         <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
